f4fd2e220d66393d11d9dfe5bf2884a92b3f55d0
Tina <TinaNguyen290@yahoo.com>
2018-09-19 11:24:18 -0700

    Added ArrangeActions Script

---
+++ UI Panels/Assets/ArrangeActions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ArrangeActions : MonoBehaviour
+{
+
+    private RectTransform panelRectTransform;
+    public GameObject thisObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        panelRectTransform = GetComponent<RectTransform>();
+        thisObject = GetComponent<GameObject>();
+    }
+
+
+}
