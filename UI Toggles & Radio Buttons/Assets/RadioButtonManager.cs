6dc121443c08e70c434ebad1aa323bf50d3eb08a
Tina Nguyen <TinaNguyen290@yahoo.com>
2018-10-05 15:08:08 -0700

    Completed UI Toggles

---
+++ UI Toggles & Radio Buttons/Assets/RadioButtonManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadioButtonManager : MonoBehaviour {
+
+    private string currentDifficulty = "Easy";
+
+    public void PrintNewGroupValue(Toggle sender)
+    {
+        if (sender.isOn)
+        {
+            currentDifficulty = sender.tag;
+            print("option changed to = " + currentDifficulty);
+        }
+    }
+
+	
+}
